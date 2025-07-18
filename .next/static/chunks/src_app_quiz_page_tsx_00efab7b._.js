835825b0575179a64d23ac39465578fbf34ab072
MONISHA30052002 <monisha30052002@gmail.com>
2025-07-18 18:21:49 +0530

    Add .next/static/chunks/src_app_quiz_page_tsx_00efab7b._.js 1752843109066

---
+++ .next/static/chunks/src_app_quiz_page_tsx_00efab7b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_5f21f736._.js",
+  "static/chunks/src_components_9b8a762a._.js"
+],
+    source: "dynamic"
+});
